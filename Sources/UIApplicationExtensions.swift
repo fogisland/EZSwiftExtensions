805970c59d6a2ea77acab3e511a16ad45ad06e76
kevin.zhu <kevin.zhu@alo7.com>
2017-02-05 19:17:09 +0800

    close uiapplication extension for unknown crash reason

--- Sources/UIApplicationExtensions.swift
+++ Sources/UIApplicationExtensions.swift
@@ -7,18 +7,18 @@
 //
 import UIKit
 
-extension UIApplication {
-    /// EZSE: Run a block in background after app resigns activity
-    public func runInBackground(closure: () -> Void, expirationHandler: (() -> Void)? = nil) {
-        dispatch_async(dispatch_get_main_queue()) {
-            let taskID: UIBackgroundTaskIdentifier
-            if let expirationHandler = expirationHandler {
-                taskID = self.beginBackgroundTaskWithExpirationHandler(expirationHandler)
-            } else {
-                taskID = self.beginBackgroundTaskWithExpirationHandler({ })
-            }
-            closure();
-            self.endBackgroundTask(taskID)
-        }
-    }
-}
+//extension UIApplication {
+//    /// EZSE: Run a block in background after app resigns activity
+//    public func runInBackground(closure: () -> Void, expirationHandler: (() -> Void)? = nil) {
+//        dispatch_async(dispatch_get_main_queue()) {
+//            let taskID: UIBackgroundTaskIdentifier
+//            if let expirationHandler = expirationHandler {
+//                taskID = self.beginBackgroundTaskWithExpirationHandler(expirationHandler)
+//            } else {
+//                taskID = self.beginBackgroundTaskWithExpirationHandler({ })
+//            }
+//            closure();
+//            self.endBackgroundTask(taskID)
+//        }
+//    }
+//}
